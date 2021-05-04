4184fdc5b0d191e9f24c1eeef41f7a3d780f25e0
Александр Захаров <qeqe@MacBook-Pro-Aleksandr.local>
2021-05-04 22:48:41 +0300

    add timezone

--- mysite/celery.py
+++ mysite/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
@@ -8,10 +9,11 @@ app = Celery('balance')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
+
 app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'update_balance': {
         'task': 'Company.tasks.update_balance',
-        'schedule': 300
+        'schedule': crontab(minute=0, hour='*/1')
     },
 }
